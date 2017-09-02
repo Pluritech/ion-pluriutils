2f6f7e86775ef135d50d000d5431c948fb4cba09
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-09-02 16:54:10 -0300

    building terms and privacy component

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from 'ionic-angular';
 import { TermsAndPrivacyComponent } from './terms-and-privacy.component';
 
 export * from './terms-and-privacy.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   declarations: [
     TermsAndPrivacyComponent
--- src/terms-and-privacy.component.ts
+++ src/terms-and-privacy.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'terms-and-privacy',
-  template: `<h1> Terms and Privacy </h1>`
+  template: `<ion-header>
+    <ion-navbar [color]="color" no-border-bottom>
+      <ion-title>
+        {{ title }}
+      </ion-title>
+    </ion-navbar>
+
+
+    <ion-toolbar no-border-top [color]="color">
+        <ion-segment [(ngModel)]="type" clear [color]="segmentcolor">
+          <ion-segment-button value="terms">
+            Termos
+          </ion-segment-button>
+          <ion-segment-button value="privacy">
+            Políticas
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
+
+  </ion-header>
+
+  <ion-content padding>
+    <div [ngSwitch]="type">
+      <div *ngSwitchCase="'terms'">
+        {{ terms }}
+      </div>
+      <div *ngSwitchCase="'privacy'">
+        {{ privacy }}
+      </div>
+    </div>
+  </ion-content>`
 })
 export class TermsAndPrivacyComponent {
+
+  public type: string = 'terms';
+  @Input() title: string = 'Termos e Políticas';
+  @Input() color: string = 'light';
+  @Input() terms: string = '';
+  @Input() privacy: string = '';
+  @Input() segmentcolor: string = 'primary';
 
   constructor() {
   }
